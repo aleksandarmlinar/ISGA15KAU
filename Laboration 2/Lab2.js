d49bc9953d4f74b0534009de4914ae3bef4eb871
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-03-22 14:34:27 +0100

    stubben

--- Laboration 2/Lab2.js
+++ Laboration 2/Lab2.js
@@ -1,4 +1,16 @@
 //Lab2.js
-'use strict'
-function validateForm() {}
-function resetForm() {}
+'use strict';
+
+
+function setFocusOnForm(){
+	console.log("setFocusOnForm()");
+  document.getElementById('cbofabrikat').focus();
+}
+
+function validateForm(){
+	console.log("validateForm()");
+
+}
+function resetForm(){
+	console.log("resetForm()");
+}
