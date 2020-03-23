af6688da61c9a9e63fe802942140e6a2820c31ff
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-03-23 12:53:11 +0100

    Updates

--- Laboration 2/Lab2.js
+++ Laboration 2/Lab2.js
@@ -5,13 +5,47 @@
 function setFocusOnForm(){
 	console.log("setFocusOnForm()");
   document.querySelector("#cbofabrikat").focus();
+	//document.getElementById("txtkommentar").disabled = true;
 }
 
 function validateForm(){
 	console.log("validateForm()");
 
+	let textRefs = document.querySelectorAll(".form-control");
+	console.log(textRefs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return false;
+
 }
 function resetForm(){
 	console.log("resetForm()");
   setFocusOnForm();
+	document.querySelector("#txtkommentar").textContent = "";
+
+}
+
+
+//textarean
+function countNrOfChars(textAreaRef){
+	console.log("countNrOfChars()");
+	let count = 5;
+	let kommentar = document.querySelector("#txtkommentar").value.length;
+	var kryss = document.querySelector("#cbokommentar");
+		if(kryss.checked == true && kommentar < 5){
+				document.querySelector("#errorMsg").innerHTML = "OBS! Du måste skriva in minst " + (count - kommentar) + " tecken till!";
+			} else {
+				document.querySelector("#errorMsg").innerHTML ="";
+		}
 }
