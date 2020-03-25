ffc2e85af441b7fc1c24362a0f0627ae1ccd0fc7
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-03-25 10:48:41 +0100

    jk

--- Laboration 2/Lab2.js
+++ Laboration 2/Lab2.js
@@ -1,47 +1,57 @@
 //Lab2.js
 'use strict';
 
-
-function setFocusOnForm(){
+/*function setFocusOnForm(){
 	console.log("setFocusOnForm()");
   document.querySelector("#cbofabrikat").focus();
-	//document.getElementById("txtkommentar").disabled = true;
+	//document.querySelector("#txtkommentar").disabled = true;
+}
+// select
+
+function showSelectInfo(formRef){
+	let selectRef = formRef.querySelector("select");
+	// console.log(selectRef);
+	// console.log(selectRef.selectedIndex);
+	// console.log(selectRef.options);
+	console.log(selectRef.options[selectRef.selectedIndex].value);
+
 }
 
-function validateForm(){
+//checkbox, bara en liten test
+/*function showCheckBoxInfo(chkBoxRef){
+	if(chkBoxRef.checked === true){
+	document.querySelector("#txtkommentar").disabled = false;
+	} else {
+	document.querySelector("#txtkommentar").disabled = true;
+	}
+}
+*/
+//validera
+function validateForm(formRef){
   //ska returnera sant eller falskt
 	console.log("validateForm()");
 
     try{
-      let textRefs = document.querySelectorAll("input[type = text], textarea");
-    	   console.log(textRefs);
-        let currentTextRef = null;
-
-          for (var counter = 0; counter < textRefs.length; counter++) {
-          currentTextRef = textRefs.item(counter);//kan använda som vector också[]
-
+			let selectRef = formRef.querySelector("select");
+      let textRefs = document.querySelectorAll("select, input[type = text], textarea");
+      let currentTextRef = null;
+          for (var i = 0; i < textRefs.length; i++) {
+          currentTextRef = textRefs.item(i);//kan använda som vektor också (textrefs[i])
           console.log(currentTextRef.getAttribute("id"));
-
             if(currentTextRef.value.length === 0){
               //throw new Error("Någon textruta saknar värde!");
-              throw {"object" : currentTextRef};
-            }
-              //String length = 5
-              //Värdet = siffror
-            if(currentTextRef !== 5){
-              throw {"object" : currentTextRef};
-            }
-            if(isNaN(currentTextRef.value)){
-              throw {"object" : currentTextRef};
+              throw {"object": currentTextRef};
             }
 
         }
 
-      return true;
-    } catch(oError){
+			return true;
+		} catch(oError){
 
         document.querySelector("#errorMsg").textContent = "Ange " + oError.object.getAttribute("title") + "!";
-        console.log(oError);
+				//document.querySelector("#errorMsg").textContent = "Du kan inte välja " + oError.object.getAttribute("select") + "!";
+				oError.object();
+				console.log(oError);
         return false;
 
 
@@ -59,17 +69,17 @@ function validateForm(){
 
 
 	return false;
-
 }
+
+	//reset
 function resetForm(){
-	console.log("resetForm()");
-  setFocusOnForm();
-	document.querySelector("#txtkommentar").textContent = "";
+//console.log("resetForm()");
+setFocusOnForm();
+document.querySelector("#errorMsg").textContent = "";
 
 }
 
-
-//textarean
+//textarea
 function countNrOfChars(textAreaRef){
 	console.log("countNrOfChars()");
 	let count = 5;
